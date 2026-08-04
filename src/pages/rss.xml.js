c336293cc09d413fb6c9b483080b8a80c305d297
Vikas Kumar  Mishra <digisoneglobal@gmail.com>
2026-08-04 18:32:05 +0530

    fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,29 @@
-import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { sanityClient } from 'sanity:client';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await sanityClient.fetch(`
+    *[_type == "post" && defined(slug.current)] | order(publishedAt desc) {
+      title,
+      "slug": slug.current,
+      excerpt,
+      publishedAt,
+      "author": author->name
+    }
+  `);
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.title,
+      description: post.excerpt || '',
+      pubDate: new Date(post.publishedAt),
+      link: `/blog/${post.slug}/`,
+      author: post.author || 'Vikas Kumar Mishra',
+    })),
+    customData: `<language>en-us</language>`,
+  });
 }
