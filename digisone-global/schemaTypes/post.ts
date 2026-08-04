1a7852960f502eae1ff5063d07fe013becca2ed2
Vikas Kumar  Mishra <digisoneglobal@gmail.com>
2026-08-04 11:33:59 +0530

    fix

---
+++ digisone-global/schemaTypes/post.ts
@@ -0,0 +1,29 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'Keep it catchy and premium.',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'URL Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt (Short Summary)',
+      type: 'text',
+    }),
+  ],
+})
