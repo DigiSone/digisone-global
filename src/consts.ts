5d812e25a733d211701ecdf04754354718b089eb
DigiSone Global <digisoneglobal@gmail.com>
2026-08-06 16:21:10 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "DigiSone Global";
-export const SITE_DESCRIPTION = "Helping Businesses, Creators, Tech Authors & Builders by providing Premium insights on AI, software engineering, startups, and modern digital ecosystem.";
+export const SITE_DESCRIPTION = "Premium insights on AI, business strategy, startups and technology.";
