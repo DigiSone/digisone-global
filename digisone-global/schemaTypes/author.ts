4b41d63c445db0e69783fbbcc2e1b0c0d1265bb7
DigiSone Global <digisoneglobal@gmail.com>
2026-08-07 06:52:35 +0530

    Update author.ts

--- digisone-global/schemaTypes/author.ts
+++ digisone-global/schemaTypes/author.ts
@@ -1,11 +1,9 @@
 import { defineField, defineType } from 'sanity'
-import { UserIcon } from '@sanity/icons'
 
 export default defineType({
   name: 'author',
   title: 'Author',
   type: 'document',
-  icon: UserIcon,
   fields: [
     defineField({
       name: 'name',
